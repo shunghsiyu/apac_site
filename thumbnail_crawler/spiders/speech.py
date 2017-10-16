f32d19fc42258339ac645b659faefbb52a1bb11e
Shung-Hsi Yu <shunghsiyu@gmail.com>
2017-10-16 14:37:43 +0800

    Update speech spider
     - Add function to obtain Youtube Video ID

--- thumbnail_crawler/spiders/speech.py
+++ thumbnail_crawler/spiders/speech.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
+import re
+
 import scrapy
 
 
 class SpeechSpider(scrapy.Spider):
     name = 'speech'
     allowed_domains = ['speech.ntu.edu.tw']
-    start_urls = ['http://speech.ntu.edu.tw/']
+    youtube_id_regex = re.compile(r'https://www.youtube.com/embed/([^?]*)?.*')
+
+    def start_requests(self):
+        return [scrapy.Request(url='http://speech.ntu.edu.tw/ntuspeech/Video/id-1837')]
 
     def parse(self, response):
-        pass
+        youtube_url = response.css('#youtube_frame::attr(src)').extract_first()
+        self.log('Got ' + self.youtube_id(youtube_url))
+
+    def youtube_id(self, url):
+        return self.youtube_id_regex.match(url).group(1)
+        
