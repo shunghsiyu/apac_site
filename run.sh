035898bf6615676d7ac2c3bf860e9bfc420181b0
Shung-Hsi Yu <shunghsiyu@gmail.com>
2017-10-20 10:41:52 +0800

    Update run.sh
     - Remove existing output.jl and output.yml
     - Store retrieved images in current directory

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-for spider in $(scrapy list); do scrapy crawl "$spider" -a input_file=urls.txt -o "output.jl"; done
+rm output.jl output.yml
+for spider in $(scrapy list);
+    do scrapy crawl "$spider" -a input_file=urls.txt -s IMAGES_STORE="$(pwd)" -o "output.jl";
+done
+python generate_yaml.py
 
