ac04884929188c0cbabd7851ef601032870bbe70
Shawn Li <tp9106@gmail.com>
2017-10-23 14:29:04 +0800

    read in yaml from script

---
+++ html_generator.rb
@@ -0,0 +1,33 @@
+require 'erb'
+require 'yaml'
+require 'json'
+
+courses_it = { id: "it", title: "資訊科技", courses: [] }
+courses_business = { id: "business", title: "商管經濟", courses: []}
+courses_asia = { id: "asia", title: "認識亞洲", courses: []}
+courses_culture = { id: "culture", title: "人文史地", courses: []}
+
+course_hashes = YAML.load(File.open("output.yml").read)
+
+course_hashes.each do |course|
+  case course["category"]
+    when "資訊科技"
+      courses_it[:courses] << course
+    when "商管經濟"
+      courses_business[:courses] << course
+    when "認識亞洲"
+      courses_asia[:courses] << course
+    when "人文史地"
+      courses_culture[:courses] << course
+  end
+end
+
+puts courses_it
+
+@courses = [courses_it, courses_business, courses_asia, courses_culture]
+
+erb = ERB.new(File.open("#{__dir__}/thai.html.erb").read)
+
+output = File.open("#{__dir__}/index.html", "w")
+output << erb.result
+output.close
